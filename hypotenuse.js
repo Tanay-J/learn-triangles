234f7bbcbd7908656b5530b572e17fb3101960a9
Tanay <tanay.joshi9@gmail.com>
2021-07-29 21:44:22 +0530

    initial commit

---
+++ hypotenuse.js
@@ -0,0 +1,12 @@
+
+const input = Array.from(document.querySelectorAll('.input-area'));
+const outputArea = document.querySelector('.output-area');
+console.log(input[1])
+input[1].addEventListener('keypress',(e) => {
+    if(e.key == 'Enter'){
+        
+        let temp = (input[0].value*input[0].value)+(input[1].value*input[1].value);
+        let hypotenuse = Math.sqrt(temp);
+        outputArea.innerHTML = `Hypotenuse of the triangle is: <span class="sub-heading">${hypotenuse}</span>`;
+    }
+})
