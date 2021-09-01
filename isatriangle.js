ba35a52da7c23293841c0a66c0f4df98b34c28e9
Tanay <tanay.joshi9@gmail.com>
2021-09-01 14:12:42 +0530

    added error message for incomplete input

--- isatriangle.js
+++ isatriangle.js
@@ -4,6 +4,12 @@ const outputArea = document.querySelector('.output-area');
 
 
 checkBtn.addEventListener('click', () => {
+    
+    if(!input[0].value || !input[1].value || !input[2].value){
+        outputArea.textContent = 'All fields are required';
+        return;
+    }
+
     if(parseInt(input[0].value)+parseInt(input[1].value)+parseInt(input[2].value) === 180){
         outputArea.textContent = 'Yes! These angles can make a triangle';
     }else outputArea.textContent = 'Nope! These angles cannot make a triangle. Sum of all angles of a triangle is 180°';
